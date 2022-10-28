940016be202368f48317b4e612ee3d47ac44d7bc
Anigbo Christopher <octan311@gmail.com>
2022-10-28 16:10:08 +0200

    init

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,8 +8,20 @@ const Profile = () => {
     <div>
         <img src={image1} id="profile_img" alt='la casa'/>
         <h1 id="header">Christopher Anigbo</h1>
-        <Button classname="w-100">Sign Up </Button>
+       
+        <div className='btn-cont'>
+        <a href='https://twitter.com/Chrstphr_Wlkngs'>Twitter Link</a>
+        <a href="https://training.zuri.team/">Zuri Team</a>
+        <a href='http://books.zuri.team'id='books'>Zuri Books </a>
+        <a href='https://books.zuri.team/' id="python_book">Python Books</a>
+        <a href='https://background.zuri.team' id="pitch">Background Check for Coders</a>
+        <a href='https://books.zuri.team/design-rules'id="design_book">Design Books </a>
+         
     </div>
+    
+
+    </div>
+    
   )
 }
 
