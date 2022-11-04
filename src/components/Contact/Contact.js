2a065859a40bf6c4f1d89b647ddaf7b4a8522593
Anigbo Christopher <octan311@gmail.com>
2022-11-04 10:06:33 +0100

    Done with html page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,38 +1,42 @@
 import React from 'react'
 import "./Contact.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Contact = () => {
   return (
-    <div className='container'>
-        <div className='Titile'>Contact Me</div>
+  <div id="body">
+      <div className='contact_container'>
+        <div className='title'>Contact Me</div>
         <form action="#">
-            <div className='"user-details'>
-                <div className='input-box'>
+            <div className='user-details'>
+                <div id="first_name">
                     <span className='details'>First Name</span>
                     <input type="text" placeholder='Enter Your First Name' required/>
                 </div>
-                <div className='input-box'>
+                <div id="last_name">
                     <span className='details'>Last Name</span>
                     <input type="text" placeholder='Enter Your Last Name' required/>
                 </div>
-                <div className='input-box'>
+                <div id='email'>
                     <span className='details'>Email</span>
                     <input type="text" placeholder='yourname@email' required/>
                 </div>
-                <div className='input-box'>
+                <div id='message'>
                     <span className='details'>Messgae</span>
                     <input type="text" placeholder="Send me a message and I'll reply you as soon as possible" required/>
                 </div>
+                
             </div>
             <div id='box'>
                 <input type= "checkbox" />
                 You agree to provide your data 
             </div>
-            <div className='button'>
+            <div id="btn__submit">
                <input type="submit" value="Submit"/> 
             </div>
         </form>
         </div>
+  </div>
   )
 }
 
